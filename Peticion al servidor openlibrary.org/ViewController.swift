ae82e9d0a2671144e56a0c0676bef90302fc2964
luiseduardo3004 <luiseduardo3004@gmail.com>
2017-04-12 00:56:03 -0500

    valida1

--- Peticion al servidor openlibrary.org/ViewController.swift
+++ Peticion al servidor openlibrary.org/ViewController.swift
@@ -10,16 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtField: UITextField!
+    @IBOutlet weak var txtView: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        txtField.clearButtonMode = .Always
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func busqueda (ISBN: String) -> NSString {
+        let urls = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:" + ISBN
+        let url = NSURL(string: urls)
+        let datos:NSData? = NSData(contentsOfURL: url!)
+        if(datos != nil){
+            let texto = NSString(data: datos!, encoding: NSUTF8StringEncoding)
+            return texto!
+        } else {
+            return "Revisa tu conexión a Internet";
+        }
+    }
 
-
+    @IBAction func ocultarTeclado(sender: UITextField){
+        sender.resignFirstResponder()
+    }
+    
+    @IBAction func presentar(sender: AnyObject) {
+        let openlibary = busqueda(String(txtField.text!));
+        txtView.text = openlibary as String
+    }
 }
 
